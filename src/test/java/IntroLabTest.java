9d3940a7117db200ee3adb99f6b2eecf008542f8
jacksonsujh <72714023+jacksonsujh@users.noreply.github.com>
2022-09-12 17:41:18 -0400

    Completed TODO

--- src/test/java/IntroLabTest.java
+++ src/test/java/IntroLabTest.java
@@ -17,7 +17,7 @@ public class IntroLabTest {
     public void tearDown() {
     }
 
-    @Test(timeout = 10)
+    @Test(timeout = 50)
     public void testExampleStudent() {
         String expected = "Hello! My name is Stu Dent! " +
                 "I like cats more than dogs! Welcome to CSC207!";
@@ -30,4 +30,9 @@ public class IntroLabTest {
      *       the test results.
      * As a reference, we've included testExampleStudent above.
      */
+    public void sujiahao() {
+        String expected = "Hi! My name is Jackson! " + "I like cats more than dogs! Nice to meet you!";
+        String actual = IntroLab.exampleStudent();
+        assertEquals(expected, actual);
+    }
 }
